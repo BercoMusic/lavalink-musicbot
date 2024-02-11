9e0e3c388489e5a143181f57be3f6b5363fe7bf4
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-02-11 13:54:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 require("dotenv/config");
 require("./src/bot");
+const { Client, Intents,Collection } = require('discord.js');
+const fs = require('fs')
+const ayarlar = require("./ayarlar.json")
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] }); 
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./komutlar').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./komutlar/${file}`);
+	client.commands.set(command.name, command);
+}
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 const express = require('express');
 const app = express();
 const port = 3000;
