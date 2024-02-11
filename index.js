ec3a5e57cd74875f4ad6f070e738c1b47eefdbd6
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-02-11 13:42:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 require("dotenv/config");
 require("./src/bot");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Web sunucu
+
+app.get('/', (req, res) => {
+
+res.sendStatus(200);
+
+});
+
+app.listen(port, () => {
+
+console.log(`Sunucu ${port} numaralı bağlantı noktasında yürütülüyor.`);
+
+});
+
+client.login(process.env.token)
